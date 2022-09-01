fbacf3858b1bf419c2ee2d1693895143ba94a875
Ronaldo Silva <ronaldojssilva@gmail.com>
2022-09-01 00:02:47 -0400

    Regras de negócio

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -1,29 +1,24 @@
 class Customer {
-    private _id: string;
-    public get id(): string {
-        return this._id;
-    }
-    public set id(value: string) {
-        this._id = value;
-    }
-    private _name: string;
-    public get name(): string {
-        return this._name;
-    }
-    public set name(value: string) {
-        this._name = value;
-    }
-    private _address: string;
-    public get address(): string {
-        return this._address;
-    }
-    public set address(value: string) {
-        this._address = value;
-    }
+    _id: string;
+    _name: string;
+    _address: string;
+    _active: boolean = true;
+
     constructor(id: string, name: string, address: string) {
         this._id = id;
         this._name = name;
         this._address = address;
     }
-    
+
+    changeName(name: string){
+        this._name = name;
+    }
+
+    activate(){
+        this._active = true;
+    }
+
+    deactivate(){
+        this._active = false;
+    }
 }
